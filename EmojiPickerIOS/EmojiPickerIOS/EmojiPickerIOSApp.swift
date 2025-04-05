d8ab21d931ed6576d6cc0e64c1df6893ae773813
Gabe Smith <gabe.smith563@gmail.com>
2025-04-05 13:49:25 -0500

    initial commit

---
+++ EmojiPickerIOS/EmojiPickerIOS/EmojiPickerIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPickerIOSApp.swift
+//  EmojiPickerIOS
+//
+//  Created by Gabe Smith on 4/5/25.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPickerIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmojiPickerIOS/EmojiPickerIOSTests/EmojiPickerIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPickerIOSTests.swift
+//  EmojiPickerIOSTests
+//
+//  Created by Gabe Smith on 4/5/25.
+//
+
+import Testing
+@testable import EmojiPickerIOS
+
+struct EmojiPickerIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
